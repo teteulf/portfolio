a633d6e6f04ef181f3484742cb940484198f68cb
teteulf <theolefevre791@gmail.com>
2024-02-02 09:13:01 -0300

    Pop up on Menu

--- src/app/menu.jsx
+++ src/app/menu.jsx
@@ -1,18 +1,17 @@
 "use client";
 import { HiUser, HiHome, HiEnvelope, HiViewColumns } from "react-icons/hi2";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const navData = [
   { name: "Home", path: "/", icon: <HiHome /> },
-  { name: "About me", path: "/About_Me", icon: <HiUser /> },
-  { name: "Talk to me", path: "/Talk_To_Me", icon: <HiEnvelope /> },
-  { name: "Projects", path: "/Projects", icon: <HiViewColumns /> },
+  { name: "About", path: "/about_me", icon: <HiUser /> },
+  { name: "Contact", path: "/talk_to_me", icon: <HiEnvelope /> },
+  { name: "Projects", path: "/projects", icon: <HiViewColumns /> },
 ];
 
 export default function Menu() {
-  const router = useRouter();
-  const pathname = router.pathname;
+  const pathname = usePathname();
   return (
     <div
       className="
@@ -27,18 +26,24 @@ export default function Menu() {
     >
       <nav className="flex xl:flex-col gap-6 ">
         {navData.map((link, index) => {
-          console.log("link.path:", link.path);
-          console.log("pathname:", pathname);
-          console.log("Condition:", link.path === pathname);
           return (
             <Link
               href={link.path}
               className={`${
                 link.path === pathname && "text-red-600"
-              } cursor-pointer`}
+              } cursor-pointer hover:text-red-600 transition-all duration-300`}
               key={index}
             >
-              <div>{link.icon}</div>
+              <div className="group flex items-center">
+                {link.icon}
+                <div className=" hidden absolute text-black right-[150%] bg-white rounded-[3px] group-hover:flex">
+                  {link.name}
+                </div>
+                <div
+                  className="hidden border-solid border-l-white border-l-8 
+                border-y-transparent border-y-[6px] border-r-0 absolute right-[130%] group-hover:flex"
+                ></div>
+              </div>
             </Link>
           );
         })}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import About from "./about_me/page";
+import Projects from "./projects/page";
+import Contact from "./talk_to_me/page";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <About />
+      <Projects />
+      <Contact />{" "}
+    </>
+  );
 }
